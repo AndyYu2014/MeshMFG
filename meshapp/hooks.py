f8e72ab471fb58bb14ff8e92cc93d580134beb85
Wu I <wu-i@b-eng.it.to-be.co.jp>
2025-07-12 21:43:33 +0900

    sync facilitymanagement workspace

--- meshapp/hooks.py
+++ meshapp/hooks.py
@@ -13,15 +13,15 @@ app_include_modules = ["erpnext_wxwork"]
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
- add_to_apps_screen = [
- 	{
- 		"name": "meshapp",
- 		"logo": "/assets/meshapp/logo.png",
- 		"title": "Meshapp",
- 		"route": "/meshapp",
- 		"has_permission": "meshapp.api.permission.has_app_permission"
- 	}
- ]
+# add_to_apps_screen = [
+# 	{
+# 		"name": "meshapp",
+# 		"logo": "/assets/meshapp/logo.png",
+# 		"title": "Meshapp",
+# 		"route": "/meshapp",
+# 		"has_permission": "meshapp.api.permission.has_app_permission"
+# 	}
+# ]
 
 # Includes in <head>
 # ------------------
